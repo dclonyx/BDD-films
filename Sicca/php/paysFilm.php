d504ef6eab04767b9fbb267ddeff225cac04c18e
Sicca <43670754+dcl-sicca@users.noreply.github.com>
2018-12-11 16:46:22 +0100

    Delete paysFilm.php

--- Sicca/php/paysFilm.php
+++
@@ -1,9 +0,0 @@
-<?php
-// On récupère le contenu table pays et table film
-$pays = $bdd->query
-('SELECT nomFilm, nomPays FROM `Film`	
-JOIN estOriginaire
-ON Film.idFilm = estOriginaire.idFilm
-JOIN pays
-ON pays.idPays = estOriginaire.idPays');
-?>
