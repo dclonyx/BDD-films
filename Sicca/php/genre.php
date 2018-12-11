d0580b91a2fced38fa086975e9b8effeb0d009b9
Sicca <43670754+dcl-sicca@users.noreply.github.com>
2018-12-11 16:46:06 +0100

    Delete genre.php

--- Sicca/php/genre.php
+++
@@ -1,8 +0,0 @@
-<?php
-// On récupère tout le contenu de la table genre
-$requestGenre = $bdd->query
-('SELECT DISTINCT nomGenre,idGenre 
-FROM genre
-WHERE nomGenre != " "
-ORDER BY nomGenre ASC');
-?>
