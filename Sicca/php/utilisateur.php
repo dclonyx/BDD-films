c04503ac000667ab0b96d831357e0d01d534cdf6
Sicca <43670754+dcl-sicca@users.noreply.github.com>
2018-12-11 16:47:19 +0100

    Delete utilisateur.php

--- Sicca/php/utilisateur.php
+++
@@ -1,7 +0,0 @@
-<?php
-// On récupère le nom des utilisateurs par film posté
-$utilisateur = $bdd->query
-('SELECT nomFilm, nomUtilisateur FROM `Film`
-JOIN utilisateur
-ON Film.idUtilisateur = utilisateur.idUtilisateur');
-?>
