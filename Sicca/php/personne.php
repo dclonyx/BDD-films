6b30caf0518a4b2479a77e3d3be98b7486c86389
Sicca <43670754+dcl-sicca@users.noreply.github.com>
2018-12-11 16:46:30 +0100

    Delete personne.php

--- Sicca/php/personne.php
+++
@@ -1,9 +0,0 @@
-<?php
-// On récuper le contenu de la table personne
-$personne = $bdd->query
-('SELECT nomFilm, nomPersonne FROM `Film`
-JOIN joue
-ON Film.idFilm = joue.idFilm
-JOIN personne
-ON personne.idPersonne = joue.idPersonne');
-?>
